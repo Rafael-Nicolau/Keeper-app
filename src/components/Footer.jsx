303f4f749a94991e9d5d0099e7ce5cef03783cc5
Rafael Nicolau <nicolaurafael@hotmail.com>
2022-04-20 16:23:10 +0200

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  const thisYear = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright © {thisYear}</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Provisory title</h1>
+      <p>Sample text</p>
+    </div>
+  );
+}
+
+export default Note;
